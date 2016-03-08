d88b5c8030ebd6f9284af49f30c89474f4406d2a
itsanti <kurov.aleksandr+github@gmail.com>
2016-03-09 00:07:59 +0300

    gunicorn test

---
+++ hello.py
@@ -0,0 +1,9 @@
+def wsgi_application(environ, start_response):
+  status = '200 OK'
+  headers = [
+    ('Content-Type', 'text/plain')
+  ]
+  qlist = environ['QUERY_STRING'].split('&')
+  body = '\r\n'.join(qlist)
+  start_response(status, headers)
+  return [ body ]

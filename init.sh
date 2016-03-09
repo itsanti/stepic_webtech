6f0e24d3240893c9ec33180d64ba28bbbf1a0d02
itsanti <kurov.aleksandr+github@gmail.com>
2016-03-09 21:45:24 +0300

    ask project start

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart

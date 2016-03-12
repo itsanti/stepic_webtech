2c04bbf4828e65b8d54e9f43261caf76086502e6
itsanti <kurov.aleksandr+github@gmail.com>
2016-03-12 23:48:02 +0300

    chmod for manage.py

--- init.sh
+++ init.sh
@@ -7,4 +7,6 @@ sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 
 sudo mysql -uroot -e "create database ask"
+
+chmod u+x /home/box/web/ask/manage.py
 /home/box/web/ask/manage.py syncdb

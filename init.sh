48e0a6e10e6d77d5109e713e3e93e37874c3e630
itsanti <kurov.aleksandr+github@gmail.com>
2016-03-12 23:34:01 +0300

    fix init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,5 @@ sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 
-sudo service mysql restart
 sudo mysql -uroot -e "create database ask"
-ask/manage.py syncdb
+sudo ask/manage.py syncdb

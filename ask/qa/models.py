6d550c0b028d5eca235fb7ca3d14f76e1d4dcb51
itsanti <kurov.aleksandr+github@gmail.com>
2016-03-17 09:35:38 +0300

    default user

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
   text = models.TextField()
   added_at = models.DateTimeField(auto_now_add=True)
   rating = models.IntegerField(default=0)
-  author = models.ForeignKey(User)
+  author = models.ForeignKey(User, default=1)
   likes = models.ManyToManyField(User, related_name='likes_set')
   
   def get_url(self):
@@ -21,4 +21,4 @@ class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question) 
-	author = models.ForeignKey(User)
+	author = models.ForeignKey(User, default=1)
